1378a6b8796444fefb388e602973f6e950b75b47
elpeyotl <ch.perez@mac.com>
2018-07-19 16:46:04 +0200

    first

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <nuxt/>
+  </div>
+</template>
+
+<style lang="scss">
+@import '~/assets/stylesheets/sass/styles.scss';
+
+
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+<div></div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+@import '~assets/stylesheets/sass/variables_mixins.scss';
+
+
+
+
+
+</style>
+
